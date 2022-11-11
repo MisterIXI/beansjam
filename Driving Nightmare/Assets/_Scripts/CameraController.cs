63936c32b7aea4b48b22e2062d3d0491cc212f72
Yannik <yannik.braendle@t-online.de>
2022-11-11 23:12:20 +0100

    first camera controller template

--- Driving Nightmare/Assets/_Scripts/CameraController.cs
+++ Driving Nightmare/Assets/_Scripts/CameraController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform _parent;
+    private Vector3 _oldPos;
+    private Vector3 _shakeTarget;
+    private float _shakeTime;
+    private float _shakeDurationFactor;
+    public float TirednessFactor = 1f;
+
     void Start()
     {
-        
+        _shakeTarget = new Vector3(0, 0, 0);
+        _parent = transform.parent;
+        _shakeDurationFactor = 1f;
+        _oldPos = transform.localPosition;
     }
 
-    // Update is called once per frame
-    void Update()
+    private float SmoothProgress(float progress)
     {
-        
+        progress = Mathf.Lerp(-Mathf.PI/2,Mathf.PI/2,progress);
+        progress = Mathf.Sin(progress);
+        progress = (progress / 2) + 0.5f;
+        return progress;
+    }
+    private void FixedUpdate()
+    {
+        // add camera sway
+        if (_shakeTime < 1f)
+        {
+            // lerp to shake target with delta time and magnitude
+            _shakeTime += Time.deltaTime;
+            transform.localPosition = Vector3.Lerp(_oldPos, _shakeTarget, SmoothProgress(_shakeTime/_shakeDurationFactor));
+
+            // decrease shake time
+        }
+        else
+        {
+            // new shake target inside sphere
+            _shakeTarget = Random.insideUnitSphere;
+            _shakeTime = 0f;
+            _oldPos = transform.localPosition;
+            _shakeDurationFactor = 1f + Vector3.Distance(_oldPos, _shakeTarget);
+        }
     }
 }
