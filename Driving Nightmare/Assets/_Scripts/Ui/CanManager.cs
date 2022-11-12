681539e00a8ac5771f4f921d537e60f0ad453b83
chocoland.tv <r.moegenburg@t-online.de>
2022-11-12 21:32:48 +0100

    SleepAbility + Mechanic + CanManagerUpdate, HUD+

--- Driving Nightmare/Assets/_Scripts/Ui/CanManager.cs
+++ Driving Nightmare/Assets/_Scripts/Ui/CanManager.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class CanManager : MonoBehaviour
 {
-    public int CanUses=3;
+    public int CanUses{get; set;}
     private int Maxuses=3;
     private int tempuse=0;
     public GameObject[] canObjects;
     // Start is called before the first frame update
     void Start()
     {
+        CanUses = 3;
         for (int i = 0; i < CanUses; i++)
         {
             canObjects[i].SetActive(true);
