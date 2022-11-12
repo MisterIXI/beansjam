7f53f63eb10412905645028f5606d6fb90267733
Yannik <yannik.braendle@t-online.de>
2022-11-12 21:05:57 +0100

    car controller working v1

--- Driving Nightmare/Assets/_Scripts/CarController.cs
+++ Driving Nightmare/Assets/_Scripts/CarController.cs
@@ -5,17 +5,20 @@ using UnityEngine.InputSystem;
 
 public class CarController : MonoBehaviour
 {
+    const float DEFAULT_SPEED = 0.5f;
+    const float MIN_SPEED = 0.2f;
     public GameObject StreetSpawner;
     public float GroundLevel = 5f;
     public float CarSpeed = 50f;
     public float SteerStrength = 20f;
     public float raycast_offset = 5f;
-
+    public float maxAngle = 80f;
     private StreetCreator _sc;
     private Rigidbody _rb;
     private EventHandler _eH;
     private float _steerInput;
     private float _gasInput;
+    private float _actualSpeed;
 
     void Start()
     {
@@ -23,6 +26,9 @@ public class CarController : MonoBehaviour
         _sc = StreetSpawner.GetComponent<StreetCreator>();
         _rb = GetComponent<Rigidbody>();
         SubscribeToEvents();
+        _gasInput = DEFAULT_SPEED;
+        _sc.ScrollSpeed = _gasInput * CarSpeed;
+
     }
 
     private void SubscribeToEvents()
@@ -33,11 +39,11 @@ public class CarController : MonoBehaviour
 
     private void SteerCallback(InputAction.CallbackContext context)
     {
-        if(context.performed)
+        if (context.performed)
         {
             _steerInput = context.ReadValue<float>();
         }
-        if(context.canceled)
+        if (context.canceled)
         {
             _steerInput = 0f;
         }
@@ -45,62 +51,83 @@ public class CarController : MonoBehaviour
 
     private void GasCallback(InputAction.CallbackContext context)
     {
-        if(context.performed)
+        if (context.performed)
         {
             _gasInput = context.ReadValue<float>();
-            _sc.ScrollSpeed = _gasInput * CarSpeed;
-        }
-        if(context.canceled)
-        {
-            _gasInput = 0.5f;
-            _sc.ScrollSpeed = 0.5f;
-        }
-    }
-
-
-    void FixedUpdate()
-    {
-        // transform.Translate(Vector3.forward * CarSpeed * Time.deltaTime);
-        // _rb.velocity = new Vector3(transform.forward.x * CarSpeed * _gasInput, _rb.velocity.y, transform.forward.z * CarSpeed * _gasInput);
-        // lerp vertical position to street height with raycast
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position - (transform.up * -1f), Vector3.down, out hit, 100f))
-        {
-            Debug.Log("Hit: " + hit.point.y);
-            float targetHeight = hit.point.y + GroundLevel;
-            // float currHeight = transform.position.y;
-            // float newHeight = Mathf.Lerp(currHeight, targetHeight, 0.5f);
-            _rb.MovePosition(new Vector3(transform.position.x, targetHeight, transform.position.z));
-            // transform.position = new Vector3(transform.position.x, targetHeight, transform.position.z);
-        }
-        // shoot two raycasts to adjust rotation
-        RaycastHit hitFront;
-        RaycastHit hitBack;
-        if (Physics.Raycast(transform.position + transform.forward * raycast_offset,Vector3.down, out hitFront, 50f))
-        {
-            if (Physics.Raycast(transform.position - transform.forward * raycast_offset, Vector3.down, out hitBack, 50f))
+            if (_gasInput < MIN_SPEED)
             {
-                Vector3 targetDir = hitFront.point - hitBack.point;
-                // targetDir.y = 0f;
-                Quaternion targetRot = Quaternion.LookRotation(targetDir);
-                _rb.MoveRotation(Quaternion.Lerp(transform.rotation, targetRot, 0.5f));
+                _gasInput = MIN_SPEED;
             }
         }
-        Debug.Log("Velocity: " + _rb.velocity);
-        _rb.velocity = new Vector3(_rb.velocity.x, 0, _rb.velocity.z);
-        Vector3 newPos = transform.position + transform.forward * CarSpeed * _gasInput * Time.deltaTime;
-        newPos.z = 0f;
-        _rb.MovePosition(newPos);
-        transform.Rotate(Vector3.up * _steerInput * Time.deltaTime * SteerStrength);
+        if (context.canceled)
+        {
+            _gasInput = DEFAULT_SPEED;
+        }
     }
 
-    private void OnDrawGizmos() {
+
+    private void Update()
+    {
+
+
+        RaycastHit hit;
+        // raycast straight down to street
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 50f))
+        {
+            float height = hit.point.y;
+            // Debug.Log("Height: " + height + " GroundLevel: " + GroundLevel);
+            // if street is below ground level, move car up
+            Vector3 lerpedPos = Vector3.Lerp(transform.position, new Vector3(transform.position.x, height + GroundLevel, transform.position.z), 0.5f);
+            transform.position = lerpedPos;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // lerp speed to desired input
+        _actualSpeed = Mathf.Lerp(_actualSpeed, _gasInput * CarSpeed, Time.deltaTime);
+        _sc.ScrollSpeed = _actualSpeed;
+        // sample two points on the street with raycasts front and back with offset
+        Vector3 front = transform.position + transform.forward * raycast_offset;
+        Vector3 back = transform.position - transform.forward * raycast_offset;
+        RaycastHit hitFront;
+        RaycastHit hitBack;
+
+        if (Physics.Raycast(front, Vector3.down, out hitFront, 50f))
+        {
+            if (Physics.Raycast(back, Vector3.down, out hitBack, 50f))
+            {
+                Vector3 direction = hitFront.point - hitBack.point;
+                // direction += Vector3.right * _steerInput * SteerStrength;
+                Quaternion rotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotation, 0.5f);
+                float angle = _steerInput * SteerStrength * Time.deltaTime;
+                float eulerAngle = transform.rotation.eulerAngles.y + maxAngle;
+                float combination = Mathf.Abs(angle + eulerAngle);
+                combination %= 360f;
+                Debug.Log("Angle: " + angle + " EulerAngles: " + eulerAngle + " Combination: " + combination);
+                if(combination > maxAngle*2)
+                {
+                    angle = 0f;
+                }
+
+                transform.RotateAround(transform.position, Vector3.up, angle);
+                // get speed forwards
+                Vector3 moveVec = transform.forward * _actualSpeed * Time.deltaTime;
+                moveVec.z = 0f;
+                transform.Translate(moveVec, Space.World);
+
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
         Gizmos.color = Color.red;
         Vector3 posF = transform.position + transform.forward * raycast_offset;
-        Vector3 posB = transform.position - transform.forward* raycast_offset;
-        Gizmos.DrawSphere(posF , 0.5f);
+        Vector3 posB = transform.position - transform.forward * raycast_offset;
+        Gizmos.DrawSphere(posF, 0.5f);
         Gizmos.DrawRay(posF, posF + transform.up * -50f);
-        Gizmos.DrawSphere(posB , 0.5f);
+        Gizmos.DrawSphere(posB, 0.5f);
         Gizmos.DrawRay(posB, posB + transform.up * -50f);
 
 
